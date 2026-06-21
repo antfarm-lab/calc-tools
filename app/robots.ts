3f44967e3c36ef63f5308c622326dec797f1c5a0
antfarm-lab <singo.fukamizu@outlook.jp>
2026-06-21 15:41:52 +0900

    fix sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://calc-tools.vercel.app/sitemap.xml",
+    sitemap: "https://calc-tools-mauve.vercel.app/sitemap.xml",
   };
 }
